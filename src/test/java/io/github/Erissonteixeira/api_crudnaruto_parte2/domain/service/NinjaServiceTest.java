edebdbc5ad508a93e338b15e0c71be5c5af2961b
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-12-21 11:54:25 -0300

    test: adicionar teste unitário para NinjaService

---
+++ src/test/java/io/github/Erissonteixeira/api_crudnaruto_parte2/domain/service/NinjaServiceTest.java
@@ -0,0 +1,23 @@
+package io.github.Erissonteixeira.api_crudnaruto_parte2.domain.service;
+
+import io.github.Erissonteixeira.api_crudnaruto_parte2.domain.contract.Ninja;
+import io.github.Erissonteixeira.api_crudnaruto_parte2.domain.entity.NinjaEntity;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class NinjaServiceTest {
+
+    @Test
+    void deveExecutarAtaqueComNinjutsu() {
+
+        NinjaService service = new NinjaService();
+        NinjaEntity ninja = new NinjaEntity("Naruto", "Konoha", 17);
+
+        Ninja resultado = service.executarAtaque("ninjutsu", ninja);
+
+        assertNotNull(resultado);
+        assertEquals("Naruto", ninja.getNome());
+    }
+}
