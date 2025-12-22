9e139fa6c5d572010f08d2303df40d387fd24ec1
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-12-22 11:40:35 -0300

    docs: adiciona schema Swagger no campo nome do NinjaRequestDto

--- src/main/java/io/github/Erissonteixeira/api_crudnaruto_parte2/domain/dto/NinjaRequestDto.java
+++ src/main/java/io/github/Erissonteixeira/api_crudnaruto_parte2/domain/dto/NinjaRequestDto.java
@@ -1,23 +1,21 @@
 package io.github.Erissonteixeira.api_crudnaruto_parte2.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 
 public class NinjaRequestDto {
-
+    @Schema(description = "Nome do ninja", example = "Naturo Uzumaki", maxLength = 30)
     @NotBlank(message = "Nome é obrigatório")
     @Size(max = 30, message = "Nome deve ter no máximo 30 caracteres")
-
     private String nome;
 
     @NotBlank(message = "Vila é obrigatória")
     @Size(max = 50, message = "Vila deve ter no máximo 50 caracteres")
-
     private String vila;
 
     @NotNull(message = "Idade é obrigatória")
     @Min(value = 0, message = "Idade não pode ser negativa")
     @Max(value = 115, message = "Idade máxima permitida é 115 anos")
-
     private Integer idade;
 
     public NinjaRequestDto() {
